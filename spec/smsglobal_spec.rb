b0c7e8d5d9b3c195beb460144217975f74d92a9c
Daniel O'Connor <daniel.oconnor@gmail.com>
2015-04-16 01:13:18 +0930

    Add a more precise expectation

--- spec/smsglobal_spec.rb
+++ spec/smsglobal_spec.rb
@@ -9,7 +9,7 @@ describe 'SmsGlobal' do
     end
 
     it "requires :user and :password" do
-      lambda { Sender.new }.should raise_error
+      lambda { Sender.new }.should raise_error('sender is required')
     end
 
     it "sends SMS correctly" do
